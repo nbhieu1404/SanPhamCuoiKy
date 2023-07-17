adffd4ccbf435e15d2eca4546f1a2a2cc352535e
Nguyen Ba hieu <nbhieu140402@gmail.com>
2023-07-17 20:39:03 +0700

    17/7

---
+++ ChucNang.js
@@ -0,0 +1,44 @@
+function dangNhap() {
+    var taiKhoan = document.getElementById('edtTaiKhoan').value;
+    var matKhau = document.getElementById('edtMatKhau').value;
+    var user = localStorage.getItem(taiKhoan);
+
+    if (taiKhoan === 'admin' && matKhau === '1') {
+        window.location.href = 'ChonKhoaHoc.html';
+    } else if (taiKhoan === '' || matKhau === '') {
+        alert("Thông tin không được để trống");
+    } else if (user === null) {
+        alert("Tài khoản không tồn tại");
+    } else {
+        var data = JSON.parse(user);
+        if (taiKhoan === data.taiKhoan && matKhau === data.matKhau) {
+            window.location.href = 'ChonKhoaHoc.html';
+        } else {
+            alert("Sai tên tài khoản hoặc mật khẩu");
+        }
+    }
+}
+function dangKy() {
+    var taiKhoan = document.getElementById('edtTaiKhoan').value;
+    var matKhau = document.getElementById('edtMatKhau').value;
+    if (taiKhoan === '' || matKhau === '') {
+        alert("Thông tin không được để trống");
+    } else {
+        var user = {
+            taiKhoan: taiKhoan,
+            matKhau: matKhau
+        };
+        var json = JSON.stringify(user);
+        localStorage.setItem(taiKhoan, json);
+        alert('Đăng ký thành công, mời bạn đăng nhập');
+    }
+}
+function tenTaiKhoan() {
+    var taiKhoan = document.getElementById('edtTaiKhoan').value;
+    var user = localStorage.getItem(taiKhoan);
+    var data = JSON.parse(user);
+
+    if (data) {
+        document.getElementById('loggedInUsername').textContent = data.taiKhoan;
+    }
+}
